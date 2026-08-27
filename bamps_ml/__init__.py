f74f08bee97390c61aa58681999c139da792a801
Ben Pascoe <benizao@gmail.com>
2026-08-27 13:59:19 +0100

    Delete bamps_ml directory

--- bamps_ml/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-BAMPS-ML stub generated 2025-06-02 by ChatGPT
-"""
-
-
-"""BAMPS-ML package — stub version"""
-from .utils import load_config
--- bamps_ml/gwas_tools.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""
-BAMPS-ML stub generated 2025-06-02 by ChatGPT
-"""
-
-"""Placeholder for GWAS utilities (STUB)."""
